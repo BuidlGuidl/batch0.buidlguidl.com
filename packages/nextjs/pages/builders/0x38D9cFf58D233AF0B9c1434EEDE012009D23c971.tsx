4d9cea5ec6f96dee946fd236157b3bb2cfc013ed
miralsuthar <miralsuthar2000@gmail.com>
2023-12-12 21:27:41 +0530

    fix: typo in bio

--- packages/nextjs/pages/builders/0x38D9cFf58D233AF0B9c1434EEDE012009D23c971.tsx
+++ packages/nextjs/pages/builders/0x38D9cFf58D233AF0B9c1434EEDE012009D23c971.tsx
@@ -21,7 +21,7 @@ const ProfilePage = () => {
     <section className="flex justify-center items-center mt-10">
       <div className="border flex gap-6 flex-col w-96 items-center border-white rounded-md p-5">
         <Address address="0x38D9cFf58D233AF0B9c1434EEDE012009D23c971" size="3xl" />
-        <p className="text-center">I am frontend engineer. Love to build stuffs onChain.</p>
+        <p className="text-center">I am frontend engineer. Love to build stuff onChain.</p>
         <div className="flex justify-around items-center w-full">
           {socials.map(social => (
             <Link className="text-blue-300 underline" key={social.title} href={social.link}>
