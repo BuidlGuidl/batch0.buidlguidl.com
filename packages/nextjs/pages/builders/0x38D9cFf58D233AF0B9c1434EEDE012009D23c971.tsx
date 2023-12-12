4b514ac2a2b0b5a9e6b2f393c376b5613579a08c
miralsuthar <miralsuthar2000@gmail.com>
2023-12-12 21:22:09 +0530

    fix: personal page

--- packages/nextjs/pages/builders/0x38D9cFf58D233AF0B9c1434EEDE012009D23c971.tsx
+++ packages/nextjs/pages/builders/0x38D9cFf58D233AF0B9c1434EEDE012009D23c971.tsx
@@ -20,12 +20,7 @@ const ProfilePage = () => {
   return (
     <section className="flex justify-center items-center mt-10">
       <div className="border flex gap-6 flex-col w-96 items-center border-white rounded-md p-5">
-        <Address
-          address="0x38D9cFf58D233AF0B9c1434EEDE012009D23c971"
-          size="3xl"
-          disableAddressLink={false}
-          format="short"
-        />
+        <Address address="0x38D9cFf58D233AF0B9c1434EEDE012009D23c971" size="3xl" />
         <p className="text-center">I am frontend engineer. Love to build stuffs onChain.</p>
         <div className="flex justify-around items-center w-full">
           {socials.map(social => (
