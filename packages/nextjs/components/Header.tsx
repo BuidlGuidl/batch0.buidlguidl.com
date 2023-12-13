8060fbda50196f6316f27c096d5279586c785876
Yan Victor <yanvictorsn@gmail.com>
2023-12-13 09:20:59 -0300

    Changed the alt prop in the Image tag

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -88,7 +88,7 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/rabbit-logo.svg" />
+            <Image alt="Rabbit logo" className="cursor-pointer" fill src="/rabbit-logo.svg" />
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Batch #0</span>
