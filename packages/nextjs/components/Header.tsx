39ece777358bdcf1076c28c25bf1ad506b7912c4
soloW <56965282+solo1g@users.noreply.github.com>
2023-12-15 23:41:47 +0530

    Add builders tab to show members (#24)

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -18,6 +18,10 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
+    label: "Builders",
+    href: "/builders",
+  },
+  {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
